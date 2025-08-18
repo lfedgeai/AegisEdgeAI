be51ba4590db58008a30340f84f6ef892af9d2f3
Ram Krishnan <ramkri123@gmail.com>
2025-08-18 15:03:40 -0700

    fixes

--- edge-ai/system-setup.sh
+++ edge-ai/system-setup.sh
@@ -1,3 +1,14 @@
+# install swtpm
 sudo apt install swtpm
+# install tpm2-tools
 sudo apt install tpm2-tools
+# install python packages
 pip install -r requirements.txt
+
+# WSL - working software versions 
+# swtpm --version
+# TPM emulator version 0.6.3, Copyright (c) 2014-2021 IBM Corp.
+# tpm2 flushcontext --version
+# tool="flushcontext" version="5.2" tctis="libtss2-tctildr" tcti-default=tcti-device
+# python --version
+# Python 3.10.12
