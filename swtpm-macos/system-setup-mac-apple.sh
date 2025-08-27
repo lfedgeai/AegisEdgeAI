824c8c7a6516107aa4c42e401b6294543a4d7119
saanvijay <saanvijay20@gmail.com>
2025-08-26 21:49:22 -0500

    macos tpm2-tools and swtpm build

--- swtpm-macos/system-setup-mac-apple.sh
+++ swtpm-macos/system-setup-mac-apple.sh
@@ -1,21 +1,51 @@
-# macos steps
-brew install cmake openssl@3 autoconf automake libtool pkg-config socat
-brew install cryptography
+brew install cmake openssl@3 autoconf automake libtool pkg-config socat cryptography
 
+brew --prefix openssl@3
 export LDFLAGS="-L$(brew --prefix openssl@3)/lib"
 export CPPFLAGS="-I$(brew --prefix openssl@3)/include"
 export PKG_CONFIG_PATH="$(brew --prefix openssl@3)/lib/pkgconfig"
 
 git clone https://github.com/stefanberger/libtpms.git
 cd libtpms
-./autogen.sh --with-tpm2 --with-openssl --prefix=/usr/local
+
+git apply libtpms.macos.apple.patch
+
+./autogen.sh --with-tpm2 --with-openssl --prefix=/opt/homebrew/opt
 make
 sudo make install
 cd ..
 
 git clone https://github.com/stefanberger/swtpm.git
 cd swtpm
-./autogen.sh --with-tpm2 --with-openssl --prefix=/usr/local
+./autogen.sh --with-tpm2 --with-openssl --prefix=/opt/homebrew/opt
 make
 sudo make install
 cd ..
+
+
+git clone https://github.com/tpm2-software/tpm2-tss.git
+cd tpm2-tss
+rm -f configure config.cache config.log
+
+git apply tpm2-tss.macos.apple.patch
+
+
+./bootstrap
+./configure --prefix=/usr/local  --disable-tcti-cmd
+make CFLAGS="-Wno-error=typedef-redefinition"
+make
+sudo make install
+
+git clone https://github.com/tpm2-software/tpm2-tools.git
+cd tpm2-tools
+
+git apply tpm2-tools.macos.apple.patch
+
+./bootstrap
+./configure --with-openssl-prefix=$(brew --prefix openssl@3) --disable-hardening 
+
+make 
+sudo make install
+ 
+
+
